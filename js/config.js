65efc785e3410cab3d3d6659d1edf803d8846d57
krishna_payyavula <45088979+KrishnaPayyavula@users.noreply.github.com>
2022-01-09 09:39:09 +0530

    Change Config to Cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '4mp4n3sdreoh6if54vg198alk1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'WildRydesWebApp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_UlBhn1Y9P', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4mp4n3sdreoh6if54vg198alk1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
